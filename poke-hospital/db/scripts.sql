6b5d3da3459f056d8da342b3a599832b7f97dd49
HHERNAND <hector.hernandez@gnp.com.mx>
2021-08-21 13:25:34 -0600

    se genera un script base del hospital de pokemon.  !!

---
+++ poke-hospital/db/scripts.sql
@@ -0,0 +1,29 @@
+insert into cat_tipo values (1, 'Planta');
+insert into cat_tipo values (2, 'Fuego');
+insert into cat_tipo values (3, 'Agua');
+insert into cat_tipo values (4, 'Bicho');
+insert into cat_tipo values (5, 'Normal');
+
+
+
+insert into pokemon values (1, 'Bulbasaur',1, false, 10);
+insert into pokemon values (2, 'Ivysaur', 1, false, 10);
+insert into pokemon values (3, 'Venusaur', 1, false, 10);
+insert into pokemon values (4, 'Charmander', 2, false, 10);
+insert into pokemon values (5, 'Charmeleon', 2, false, 10);
+insert into pokemon values (6, 'Charizard', 2, false, 10);
+insert into pokemon values (7, 'Squirtle', 3, false, 10);
+insert into pokemon values (8, 'Wartortle', 3, false, 10);
+insert into pokemon values (9, 'Blastoise', 3, false, 10);
+insert into pokemon values (10,'Caterpie', 4, false, 10);
+insert into pokemon values (11,'Metapod', 4, false, 10);
+insert into pokemon values (12,'Butterfree', 4, false, 10);
+insert into pokemon values (13,'Weedle', 4, false, 10);
+insert into pokemon values (14,'Kakuna', 4, false, 10);
+insert into pokemon values (15,'Beedrill', 4 , false, 10);
+insert into pokemon values (16,'Pidgey', 5, false, 10);
+insert into pokemon values (17,'Pidgeotto', 5 , false, 10);
+insert into pokemon values (18,'Pidgeot', 5, false, 10);
+insert into pokemon values (19,'Rattata', 5, false, 10);
+
+
